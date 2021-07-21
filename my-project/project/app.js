6a10dea0f4ab20cddaa59ceeff0edff15ace0f29
Alen <alechkr@gmail.com>
2021-07-21 11:38:33 +0200

    v-for

--- my-project/project/app.js
+++ my-project/project/app.js
@@ -2,8 +2,13 @@ new Vue({
   el: "#App",
   data: {
     title: "hello world!",
+    // v-if
     items: [1, 2, 3],
     message: "Type something in",
+    // v-model
     checkedNames: [],
+    // v-bind
+    pie: "",
+    activeClass: "active",
   },
 });
