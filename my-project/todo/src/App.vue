181baf11ea38f364cdea7a46722da308626f1f59
Alen <alechkr@gmail.com>
2021-08-03 09:28:44 +0200

    a todo app in vue2

---
+++ my-project/todo/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
