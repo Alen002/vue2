01586231299657c7c36ee1e8bc39f4f8bb96b767
Alen <alechkr@gmail.com>
2021-08-03 10:54:37 +0200

    added sass-loader and node-sass

--- my-project/todo/src/App.vue
+++ my-project/todo/src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png" />
-    <HelloWorld />
+  <div id="app" class="container">
+    <img src="./assets/logo.png" class="logo" />
+    <ToDoList />
+    <!-- <HelloWorld /> -->
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld";
+import ToDoList from "./components/ToDoList";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    ToDoList
   }
 };
 </script>
@@ -24,12 +27,19 @@ export default {
 .container {
   max-width: 600px;
   margin: 0 auto;
+  background: lightblue;
 }
+
+.logo {
+  width: 50px;
+  height: 50px;
+}
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ my-project/todo/src/components/ToDoList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="todo">
+    <h3>
+      {{ rsp }}
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "todo-list",
+  data() {
+    return {
+      rsp: "this is the todo list"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss"></style>
